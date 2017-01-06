ea8d04e590f0a9675598c6d875b49c9e276c976c
Mitchell Oleson <moleson21@berkeley.edu>
2017-01-05 18:08:47 -0800

    Networking updated for UDP

--- Networking/Client.py
+++ Networking/Client.py
@@ -1,64 +1,23 @@
-# import cv2
-# import numpy as np
-# import socket
-# import sys
-# import pickle
-# import struct
-# import errno
-#
-# HOST = '127.0.0.1'
-# PORT = 8083
-#
-#
-# cap = cv2.VideoCapture(0)
-#
-#
-# clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#
-# # clientsocket.connect((HOST, PORT))
-#
-# clientsocket.bind((HOST, PORT))
-#
-# clientsocket.listen(10)
-#
-# while True:
-#     client, address = clientsocket.accept()
-#     ret, frame = cap.read()
-#     if ret:
-#         frame = frame.flatten()
-#         data = frame.tostring()
-#         clientsocket.send(data)
-#     # data = pickle.dumps(frame)
-#     # clientsocket.sendall(struct.pack("L", len(data)) + data)
-#
-# clientsocket.close()
-
-
 import socket
 import cv2
 import numpy
+import sys
 
-TCP_IP = '0.0.0.0'
-TCP_PORT = 5001
-
-sock = socket.socket()
-sock.connect((TCP_IP, TCP_PORT))
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.connect(('127.0.0.1', 3000))
 
 capture = cv2.VideoCapture(1)
 
 while True:
     ret, frame = capture.read()
-    encode_param=[int(cv2.IMWRITE_JPEG_QUALITY),90]
+    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY),50]
     result, imgencode = cv2.imencode('.jpg', frame, encode_param)
     data = numpy.array(imgencode)
     stringData = data.tostring()
 
-    sock.send( str(len(stringData)).ljust(16));
-    sock.send(stringData);
+    try:
+        sock.send(stringData);
+    except socket.error:
+        print("Too Large: " + str(len(stringData)))
 
 sock.close()
-
-decimg=cv2.imdecode(data,1)
-cv2.imshow('CLIENT',decimg)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
--- Networking/Server.py
+++ Networking/Server.py
@@ -1,89 +1,20 @@
-# import socket
-# import sys
-# import cv2
-# import pickle
-# import numpy
-# import struct
-# from PIL import Image, ImageTk
-#
-# def recvall(sock, count):
-#     buf = b''
-#     while count:
-#         newbuf = sock.recv(count)
-#         if not newbuf: return None
-#         buf += newbuf
-#         count -= len(newbuf)
-#     return buf
-#
-# HOST = '127.0.0.1'
-# PORT = 8083
-#
-# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# print 'Socket created'
-#
-# s.bind((HOST, PORT))
-# print 'Socket bind complete'
-# s.listen(10)
-# print 'Socket now listening'
-#
-# conn, addr = s.accept()
-#
-# data = ""
-# payload_size = struct.calcsize("L")
-#
-# while True:
-#     # while len(data) < payload_size:
-#     #     data += conn.recv(4096)
-#     # packed_msg_size = data[:payload_size]
-#     # data = data[payload_size:]
-#     # msg_size = struct.unpack("L", packed_msg_size)[0]
-#     # while len(data) < msg_size:
-#     #     data += conn.recv (4096)
-#     # frame_data = data[:msg_size]
-#     # data = data[msg_size:]
-#     #
-#     # frame = pickle.loads(frame_data)
-#     # cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
-#     # img = Image.fromarray(cv2image)
-#     # imgtk = ImageTk.PhotoImage(image=img)
-#     data = s.recv()
-#     print(data)
-#     cv2.imshow('frame', data)
-#
-# s.close()
-
 import socket
 import cv2
 import numpy
 
-def recvall(sock, count):
-    buf = b''
-    while count:
-        newbuf = sock.recv(count)
-        if not newbuf: return None
-        buf += newbuf
-        count -= len(newbuf)
-    return buf
-
-TCP_IP = '0.0.0.0'
-TCP_PORT = 5001
-
-# HOST = '68.108.243.166'
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((TCP_IP, TCP_PORT))
-# s.connect((HOST, TCP_PORT))
-s.listen(True)
-conn, addr = s.accept()
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(('', 3000))
+recv_len = 64000*8
 
 while True:
-    length = recvall(conn,16)
-    stringData = recvall(conn, int(length))
-    data = numpy.fromstring(stringData, dtype='uint8')
-    s.close()
-
-    decimg=cv2.imdecode(data,1)
-    cv2.imshow('SERVER',decimg)
-    cv2.waitKey(1)
+    try:
+        msg = sock.recv(recv_len)
+        data = numpy.fromstring(msg, dtype='uint8')
+        decimg = cv2.imdecode(data,1)
+        cv2.imshow('SERVER', decimg)
+        cv2.waitKey(1)
+    except socket.error:
+        print("Received Too Large")
 
 cv2.destroyAllWindows()
+sock.close()
