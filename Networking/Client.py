8b7be2b9c78d0aac0413ca1ca075adc59081c4c2
Mitchell Oleson <moleson21@berkeley.edu>
2017-01-06 22:07:04 -0800

    Networking command line functionaility

--- Networking/Client.py
+++ Networking/Client.py
@@ -3,21 +3,28 @@ import cv2
 import numpy
 import sys
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.connect(('127.0.0.1', 3000))
+# Command line arg list
+# [ip_addr ip_port cam_num]
 
-capture = cv2.VideoCapture(1)
+if len(sys.argv) < 4:
+	print("Error not enough arguments!")
+	exit(1)
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.connect((sys.argv[1], int(sys.argv[2])))
+
+capture = cv2.VideoCapture(int(sys.argv[3]))
 
 while True:
     ret, frame = capture.read()
-    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY),50]
+    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY),90]
     result, imgencode = cv2.imencode('.jpg', frame, encode_param)
     data = numpy.array(imgencode)
     stringData = data.tostring()
 
     try:
         sock.send(stringData);
-    except socket.error:
-        print("Too Large: " + str(len(stringData)))
+    except socket.error as e:
+    	print(e)
 
 sock.close()
